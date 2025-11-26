f202a4899634642d48afaa0e92849a81bbdcf363
Roberto Jimenez Sanchez <roberto.jimenez@grafana.com>
2025-11-26 14:40:20 +0100

    Consolidate set up scripts.

--- scenario-1-default/setup-git-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Call common setup script
-../scripts/setup-git-sync.sh scenario-1-default \
-    default repository.yaml
-
-echo ""
-echo "Grafana: http://localhost:3000 - syncing from scenario-1-default/grafana"
